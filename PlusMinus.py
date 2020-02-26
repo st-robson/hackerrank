bfaf4520fc2152747054b53f77b65f3ac42d9a44
Robson Oliveira <robson@solutiontrue.com.br>
2020-02-26 16:38:52 -0300

    PlusMinus wip

--- PlusMinus.py
+++ PlusMinus.py
@@ -1,7 +1,13 @@
 #! /bin/python3
 
+from array import *
+
 def plusMinus(arr):
-    print("hello")
+    divvy = len(arr)
+    negatives = array('i',filter(lambda x: x < 0, arr))
+    zeros = array('i', filter(lambda x: x == 0, arr))
+    positives = array('i',filter(lambda x: x > 0, arr))
+    return array('f', [(len(positives)/divvy), (len(negatives)/divvy), (len(zeros)/divvy)])
 
 if __name__ == "__main__":
-    plusMinus([1, 2 ,3])
+    print(plusMinus(array('i',[1, 1 ,0 , -1, -1])))

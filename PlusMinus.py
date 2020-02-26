e390d4a355293ced46648ac9e0fb609884a9bc87
Robson Oliveira <robson@solutiontrue.com.br>
2020-02-26 20:31:54 -0300

    ADD: Hackerrank PlusMinus chalange solved

--- PlusMinus.py
+++ PlusMinus.py
@@ -1,13 +1,16 @@
 #! /bin/python3
 
 from array import *
+import math
 
 def plusMinus(arr):
     divvy = len(arr)
     negatives = array('i',filter(lambda x: x < 0, arr))
     zeros = array('i', filter(lambda x: x == 0, arr))
     positives = array('i',filter(lambda x: x > 0, arr))
-    return array('f', [(len(positives)/divvy), (len(negatives)/divvy), (len(zeros)/divvy)])
+    print(round((len(positives)/divvy), 6)) 
+    print(round((len(negatives)/divvy), 6)) 
+    print(round((len(zeros)/divvy), 6)) 
 
 if __name__ == "__main__":
-    print(plusMinus(array('i',[1, 1 ,0 , -1, -1])))
+    plusMinus(array('i',[1, 1 ,0 , -1, -1]))
